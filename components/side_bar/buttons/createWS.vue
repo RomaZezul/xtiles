eef7d9931f085ca1087c7d86b6ec39609ad6c414
RomaZezul <romazezul@gmail.com>
2022-07-18 00:11:57 +0300

    sidebar design, scss,

--- components/side_bar/buttons/createWS.vue
+++ components/side_bar/buttons/createWS.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="createWS">
-      Workspace
-      <button id="show-modal" @click="SETshowModal(true)">+</button>
-    </div>
+    Workspace
+    <button id="show-modal" @click="SETshowModal(true)">+</button>
+  </div>
 </template>
    
 
 <script>
 export default {
-
   methods: {
     SETshowModal(b) {
-      this.$store.commit('workspace/SET_SHOW_MODAL', b);
+      this.$store.commit("workspace/SET_SHOW_MODAL", b);
     },
   },
-}
+};
 </script>
 <style>
-.createWS{
+.createWS {
   margin: 30px;
 }
 </style>
