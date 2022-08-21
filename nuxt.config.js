bf51ed07059e78e8c494fc600ecffb9c770043d7
RomaZezul <romazezul@gmail.com>
2022-08-21 23:28:54 +0300

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,7 +66,8 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     //baseURL: 'https://xtiels.azurewebsites.net',
         //baseURL: 'https://localhost:44347',
-        baseURL: 'http://xTiles.somee.com/PUBLISCH',
+        //baseURL: 'http://xTiles.somee.com/PUBLISCH',
+        baseURL: 'http://workspace.somee.com',
 
   },
 
